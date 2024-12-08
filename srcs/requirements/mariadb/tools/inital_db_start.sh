a748b3eaef9b791744616a9eb4b45a59aff6a3ac
senemyilmazz <senemy97@gmail.com>
2024-12-08 19:10:23 +0300

    mariadb Dockerfile

--- srcs/requirements/mariadb/tools/inital_db_start.sh
+++ srcs/requirements/mariadb/tools/inital_db_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
+
 envsubst < /var/www/initial_db_temp.sql > /var/www/initial_db.sql
 
-mysql -u root -proot12345 < /var/www/initial_db.sql
+mysql -u ${DB_ROOT_USER} -p${DB_ROOT_PASSWORD} < /var/www/initial_db.sql
 
 rm -f /var/www/initial_db.sql
