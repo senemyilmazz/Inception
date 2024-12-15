ebd9501f8f5a5cdd6aac770965a4159fd4bd3907
senemyilmazz <senemy97@gmail.com>
2024-12-15 09:17:29 +0300

    inception

--- srcs/requirements/mariadb/tools/inital_db_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-envsubst < /var/www/initial_db_temp.sql > /var/www/initial_db.sql
-
-mysql -u ${DB_ROOT_USER} -p${DB_ROOT_PASSWORD} < /var/www/initial_db.sql
-
-rm -f /var/www/initial_db.sql
---
+++ srcs/requirements/wordpress/tools/create-wp.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Eğer WordPress yapılandırma dosyası yoksa işlemi başlat
+if [ ! -f ./wp-config.php ]; then
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz  wordpress
+
+	cd /var/www/html
+
+    wp cli update
+    wp config create --dbname="${DB_NAME}" --dbuser="${DB_USERNAME}" --dbpass="${DB_USERPASS}" --dbhost="${DB_HOST}" --force --allow-root
+    wp core install --allow-root --url="${DOMAIN}" --title="${WP_TITLE}" --admin_user="${WP_ADMINNAME}" --admin_password="${WP_ADMINPASS}" --admin_email="${WP_ADMINMAIL}"
+    wp user create --allow-root "${WP_USERNAME}" "${WP_USERMAIL}" --role="${WP_USERROLE}" --user_pass="${WP_USERPASS}"
+fi
+
+# Eklentileri güncelle
+wp plugin update --all --allow-root
+
+# PHP-FPM başlat
+exec "$@"
+
